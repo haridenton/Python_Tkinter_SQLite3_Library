fcd6a8beb0c8e2a9982adfe4118a9ada63c24630
hdenton <SteamSucks2020>
2021-04-23 11:22:30 +0800

    Added Sonic and Finn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sqlite3
-from tkinter import *
-from tkinter import messagebox
-
+import tkinter
 
 
 def nav_forward():
@@ -69,9 +67,9 @@ def new():
 
 
 def delete():
-    MsgBox = messagebox.askquestion('DELETE RECORD',
-                                    'Are you sure you want to delete the current record? This cannot be undone.',
-                                    icon='warning')
+    MsgBox = tkinter.messagebox.askquestion('DELETE RECORD',
+                                            'Are you sure you want to delete the current record? This cannot be undone.',
+                                            icon='warning')
     if MsgBox == 'yes':
         conn = sqlite3.connect('library.db')
         c = conn.cursor()
@@ -84,51 +82,51 @@ def delete():
     print("DELETED")
 
 
-root = Tk()
+root = tkinter.Tk()
 root.geometry("500x500")
 root.title("Hello World GUI")
 
 # CREATE INDEX FOR OUR RETURNED DATA SET
-var_recordset_index = IntVar()
+var_recordset_index = tkinter.IntVar()
 var_recordset_index.set(0)
-var_rowcount = IntVar()
+var_rowcount = tkinter.IntVar()
 
-var_book_id = StringVar(root)
-var_title = StringVar(root)
-var_genre = StringVar(root)
-var_published_date = StringVar(root)
+var_book_id = tkinter.StringVar(root)
+var_title = tkinter.StringVar(root)
+var_genre = tkinter.StringVar(root)
+var_published_date = tkinter.StringVar(root)
 
-book_window_label = Label(root, text="Library System: BOOKS")
+book_window_label = tkinter.Label(root, text="Library System: BOOKS")
 book_window_label.grid(row=0, column=0, columnspan=2)
-book_id = Label(root, text="Book ID:")
+book_id = tkinter.Label(root, text="Book ID:")
 book_id.grid(padx=20, pady=20, row=1, column=0)
-book_id_entry = Entry(root, textvariable=var_book_id, bd=3)
+book_id_entry = tkinter.Entry(root, textvariable=var_book_id, bd=3)
 book_id_entry.grid(padx=20, pady=20, row=1, column=1)
-title = Label(root, text="Title:")
+title = tkinter.Label(root, text="Title:")
 title.grid(padx=20, pady=20, row=2, column=0)
-title_entry = Entry(root, textvariable=var_title, bd=3)
+title_entry = tkinter.Entry(root, textvariable=var_title, bd=3)
 title_entry.grid(padx=20, pady=20, row=2, column=1)
-genre = Label(root, text="Genre:")
+genre = tkinter.Label(root, text="Genre:")
 genre.grid(padx=20, pady=20, row=3, column=0)
-genre_entry = Entry(root, textvariable=var_genre, bd=3)
+genre_entry = tkinter.Entry(root, textvariable=var_genre, bd=3)
 genre_entry.grid(padx=20, pady=20, row=3, column=1)
-published_date = Label(root, text="Published Date:")
+published_date = tkinter.Label(root, text="Published Date:")
 published_date.grid(padx=20, pady=20, row=4, column=0)
-published_date_entry = Entry(root, textvariable=var_published_date, bd=3)
+published_date_entry = tkinter.Entry(root, textvariable=var_published_date, bd=3)
 published_date_entry.grid(padx=20, pady=20, row=4, column=1)
 
-nav_backward_btn = Button(root, text="<", command=nav_backward)
+nav_backward_btn = tkinter.Button(root, text="<", command=nav_backward)
 nav_backward_btn.grid(row=6, column=0)
-nav_forward_btn = Button(root, text=">", command=nav_forward)
+nav_forward_btn = tkinter.Button(root, text=">", command=nav_forward)
 nav_forward_btn.grid(row=6, column=3)
 
-insert_btn = Button(root, text="INSERT", command=insert, state="disabled")
+insert_btn = tkinter.Button(root, text="INSERT", command=insert, state="disabled")
 insert_btn.grid(row=7, column=0)
-update_btn = Button(root, text="UPDATE", command=update)
+update_btn = tkinter.Button(root, text="UPDATE", command=update)
 update_btn.grid(row=7, column=1)
-delete_btn = Button(root, text="DELETE", command=delete)
+delete_btn = tkinter.Button(root, text="DELETE", command=delete)
 delete_btn.grid(row=7, column=2)
-new_btn = Button(root, text="NEW", command=new)
+new_btn = tkinter.Button(root, text="NEW", command=new)
 new_btn.grid(row=7, column=3)
 
 set_data()
